16dabcb003f50b8a6410802bf963262eda3b1295
Arnab Mukhopadhyay <arnabmukhopadhyay76@gmail.com>
2022-11-16 18:33:30 +0530

    Basic Data Types

---
+++ 3.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    long l;
+    char c;
+    float f;
+    double d;
+    scanf("%d %ld %c %f %lf", &a, &l, &c, &f, &d);
+    printf("%d\n%ld\n%c\n%f\n%lf\n", a, l, c, f, d);
+    return 0;
+}
