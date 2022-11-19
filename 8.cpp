79d0360f83c82b93331644bca87939b475b93810
Arnab Mukhopadhyay <arnabmukhopadhyay76@gmail.com>
2022-11-19 22:31:00 +0530

    Arrays Introduction

--- 8.cpp
+++ 8.cpp
@@ -10,12 +10,12 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
     int n;
     cin>>n;
-    vector<int> v;
-    for(int i = 0; i< n;i++){
-        int t;
-        cin>>t;
-        v.push_back(t);
-    }
+    // vector<int> v;
+    // for(int i = 0; i< n;i++){
+    //     int t;
+    //     cin>>t;
+    //     v.push_back(t);
+    // }
     // METHOD 1
     // for(int i = v.size() - 1; i >= 0; i--){
     //     cout<<v[i]<<" ";
@@ -23,10 +23,20 @@ int main() {
     // cout<<endl;
     
     // METHOD 2
-    reverse(v.begin(), v.end());
-    for(auto i: v){
-        cout<<i<<" ";
-    } 
+    // reverse(v.begin(), v.end());
+    // for(auto i: v){
+    //     cout<<i<<" ";
+    // } 
+    // cout<<endl;
+    
+    // USING ARRAY
+    int arr[n];
+    for(int i = 0;i < n;i++){
+        cin>>arr[i];
+    }
+    for(int i = n-1;i >= 0;i--){
+        cout<<arr[i]<<" ";
+    }
     cout<<endl;
     return 0;
 }
